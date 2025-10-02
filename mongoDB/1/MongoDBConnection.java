c6be55af481cd0e0eee97496200bfdc0938244c4
zsantana <reinaldojsantana@gmail.com>
2025-10-02 13:24:11 +0000

    mongo ex 2

--- mongoDB/1/MongoDBConnection.java
+++ mongoDB/1/MongoDBConnection.java
@@ -7,8 +7,14 @@ import org.bson.Document;
 public class MongoDBConnection {
 
     public static MongoCollection<Document> getUsuarioCollection() {
-        MongoClient mongoClient = new MongoClient("localhost", 27017); // Ajuste se necessário
-        MongoDatabase database = mongoClient.getDatabase("test_db");    // Use seu nome de banco
-        return database.getCollection("usuarios");
+        try {
+            MongoClient mongoClient = new MongoClient("localhost", 27017); // Ajuste se necessário
+            MongoDatabase database = mongoClient.getDatabase("test_db");    // Use seu nome de banco
+            System.out.println("Conexão estabelecida com sucesso ao MongoDB!");
+            return database.getCollection("usuarios");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
