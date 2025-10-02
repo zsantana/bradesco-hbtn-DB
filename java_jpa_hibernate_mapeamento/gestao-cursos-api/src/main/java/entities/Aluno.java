783731237be797150eeff36c2dff97014aa67006
zsantana <reinaldojsantana@gmail.com>
2025-10-02 12:47:24 +0000

    Hibernate exemplo 2

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
@@ -1,6 +1,7 @@
 package entities;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.*;
 
 @Entity
@@ -9,16 +10,45 @@ public class Aluno {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome;
+    private String nomeCompleto;
+    private String matricula;
 
-    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL)
+    private LocalDate nascimento;
+
+    private String email;
+
+    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Telefone> telefones = new ArrayList<>();
 
-    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ManyToMany(mappedBy = "alunos")
     private List<Curso> cursos = new ArrayList<>();
 
-    // getters e setters
+    // Getters e Setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNomeCompleto() { return nomeCompleto; }
+    public void setNomeCompleto(String nomeCompleto) { this.nomeCompleto = nomeCompleto; }
+
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+
+    public LocalDate getNascimento() { return nascimento; }
+    public void setNascimento(LocalDate nascimento) { this.nascimento = nascimento; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public List<Telefone> getTelefones() { return telefones; }
+    public void setTelefones(List<Telefone> telefones) { this.telefones = telefones; }
+
+    public List<Endereco> getEnderecos() { return enderecos; }
+    public void setEnderecos(List<Endereco> enderecos) { this.enderecos = enderecos; }
+
+    public List<Curso> getCursos() { return cursos; }
+    public void setCursos(List<Curso> cursos) { this.cursos = cursos; }
 }
--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
@@ -9,10 +9,26 @@ public class Professor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome;
+    private String nomeCompleto;
+    private String matricula;
+    private String email;
 
-    @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Curso> cursos = new ArrayList<>();
 
-    // getters e setters
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNomeCompleto() { return nomeCompleto; }
+    public void setNomeCompleto(String nomeCompleto) { this.nomeCompleto = nomeCompleto; }
+
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public List<Curso> getCursos() { return cursos; }
+    public void setCursos(List<Curso> cursos) { this.cursos = cursos; }
 }
