8c8b9c2a6fc7af2c16573e3ef3e02f0afb5577ce
zsantana <reinaldojsantana@gmail.com>
2025-10-02 12:30:22 +0000

    Hibernate exemplo 1

--- jpa_hibernate/src/main/java/entities/Pessoa.java
+++ jpa_hibernate/src/main/java/entities/Pessoa.java
@@ -1,5 +1,4 @@
 package entities;
-
 import javax.persistence.*;
 
 @Entity
@@ -10,14 +9,15 @@ public class Pessoa {
     private Long id;
     private String nome;
     private String email;
+    private Integer idade; // Adicionado o campo idade
 
     // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+    public Integer getIdade() { return idade; }           // Getter para idade
+    public void setIdade(Integer idade) { this.idade = idade; } // Setter para idade
 }
